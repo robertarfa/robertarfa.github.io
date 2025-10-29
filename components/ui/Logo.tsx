eea64a17328a287f1ccded4bd4e79a45edb11d2b
Roberta Aguilar <roberta_rfa@hotmail.com>
2025-10-28 23:31:59 -0300

    Fix

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -7,8 +7,16 @@ interface LogoProps {
 
 export default function Logo({ className = '' }: LogoProps) {
   return (
-    <Link href="/" className={`text-2xl font-bold text-orange-500 no-underline ${className}`}>
-      <Image src="/layout/logo.svg" alt="CollabTranslation Logo" width={150} height={50} />
+    <Link
+      href='/'
+      className={`text-2xl font-bold text-orange-500 no-underline ${className}`}
+    >
+      <Image
+        src='/layout/logo.svg'
+        alt='CollabTranslation Logo'
+        width={150}
+        height={50}
+      />
     </Link>
   );
 }
